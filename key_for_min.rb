89b01f441894044658ccf232d3a67c22fb8fcc08
Javier Aguilar <coyote176@msn.com>
2020-08-08 14:55:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,8 +11,7 @@
 def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
-  
-  name_hash.each do |key,value|
+    name_hash.each do |key,value|
     #if the lowest_value is in it's intial state  
     #or if the value of the current key is less than the lowest_value
     #then: set the lowest_value to value and lowest_key to key
